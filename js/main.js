4d1d56166fc8d8ed9e694ba27980f4726c23e6e6
Latens Tech <132476701+LatensTech@users.noreply.github.com>
2025-11-02 18:31:53 +0300

    Delete js directory

--- js/main.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('contactForm').addEventListener('submit', function(e) {
-    e.preventDefault();
-    const formResponse = document.getElementById('formResponse');
-    formResponse.textContent = "Thanks! Your message has been received (Test Mode).";
-    this.reset();
-});
